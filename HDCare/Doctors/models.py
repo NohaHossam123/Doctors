69cdba328e0ab0da116bb2a5f4435477d53ecbf5
NohaHossam123 <nohahossam38@gmail.com>
2020-06-01 23:12:34 +0200

    doctors model

--- HDCare/Doctors/models.py
+++ HDCare/Doctors/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.core.validators import RegexValidator , MaxValueValidator , MinValueValidator
+from users.models import User
 
-# Create your models here.
+class Doctor_Book(models.Model):
+    fees = models.IntegerField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    waiting_time = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.waiting_time
+
+class Doctor(models.Model):
+    image = models.ImageField(upload_to = 'doctors/images')
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    phone_regex = RegexValidator(regex=r'^[\+2]?(01)(0|1|2|5)([0-9]{8})$', message="the format of phone number must be as : +20 111 111 1111")
+    phone = models.CharField(validators=[phone_regex], max_length=13)
+    specialization = models.CharField(max_length=60)
+    bio = models.TextField()
+    clinic_address = models.CharField(max_length=100)
+    book = models.ForeignKey(Doctor_Book, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name}{self.last_name}"
+
+class Comment(models.Model):
+    context = models.TextField(blank=True , null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.context
+
+class Rate(models.Model):
+    rate = models.IntegerField(validators=[MaxValueValidator(1), MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Complain(models.Model):
+    contain = models.TextField(blank=True , null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.contain
