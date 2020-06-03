ea6f3ccfa8c34aaa7e1675d0b1a9cd8001336870
NohaHossam123 <nohahossam38@gmail.com>
2020-06-03 12:59:04 +0200

    modify in doctor model

--- HDCare/Doctors/models.py
+++ HDCare/Doctors/models.py
@@ -2,29 +2,36 @@ from django.db import models
 from django.core.validators import RegexValidator , MaxValueValidator , MinValueValidator
 from users.models import User
 
+
+class Doctor(models.Model):
+    image = models.ImageField(null=True, blank=True)
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    phone_regex = RegexValidator(regex=r'^[\+2]?(01)(0|1|2|5)([0-9]{8})$', message="the format of phone number must be as : +20 111 111 1111")
+    phone = models.CharField(validators=[phone_regex], max_length=13, unique=True)
+    specialization = models.CharField(max_length=60)
+    bio = models.TextField()
+    clinic_address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name}{self.last_name}"
+
 class Doctor_Book(models.Model):
     fees = models.IntegerField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    waiting_time = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    waiting_time = models.CharField(max_length=20)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.waiting_time
 
-class Doctor(models.Model):
-    image = models.ImageField(upload_to = 'doctors/images')
-    first_name = models.CharField(max_length=60)
-    last_name = models.CharField(max_length=60)
-    phone_regex = RegexValidator(regex=r'^[\+2]?(01)(0|1|2|5)([0-9]{8})$', message="the format of phone number must be as : +20 111 111 1111")
-    phone = models.CharField(validators=[phone_regex], max_length=13)
-    specialization = models.CharField(max_length=60)
-    bio = models.TextField()
-    clinic_address = models.CharField(max_length=100)
-    book = models.ForeignKey(Doctor_Book, on_delete=models.CASCADE)
+class UserBook(models.Model):
+    doctor_book = models.ForeignKey(Doctor_Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f"{self.first_name}{self.last_name}"
+    class Meta:
+        unique_together = ('doctor_book','user')
 
 class Comment(models.Model):
     context = models.TextField(blank=True , null=True)
@@ -33,6 +40,9 @@ class Comment(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        unique_together = ('doctor','user')
+    
     def __str__(self):
         return self.context
 
@@ -42,6 +52,10 @@ class Rate(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('doctor','user')
+
 
 class Complain(models.Model):
     contain = models.TextField(blank=True , null=True)
