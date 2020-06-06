e3085b325a74f7f688b9923aaa1016174d12dcb5
NohaHossam123 <nohahossam38@gmail.com>
2020-06-06 03:20:29 +0200

    finishing activate user

--- HDCare/users/urls.py
+++ HDCare/users/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('logout/', user_logout, name='logout'),
     path('', home, name='home'),
     path('appointments/', appointments, name='appointments'),
-    path('<str:token>', activate_account, name="activate"),
+    path('activate/<str:token>', activate_account, name="activate"),
 ]
